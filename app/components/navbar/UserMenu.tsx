56976cfbd02627a5040fb66da49e1edf04f230e4
CalebScott0 <calebbenscott@gmail.com>
2024-10-09 21:19:31 -0500

    adding useRef to userMenu and document event listener to close menu on click outside of ref

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,17 +2,45 @@
 
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
-import { useCallback, useState } from "react";
+import { useCallback, useState, useRef, useEffect } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 const UserMenu = () => {
   const registerModal = useRegisterModal();
 
+  // explicit type of either HtmlDivElement or null to remove error .contains
+  // checks if null before calling contains
+  const dropDownRef = useRef<HTMLDivElement | null>(null);
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
+  }, []);
+
+  // use effect to add event listener on component mount and remove when unmounted
+  useEffect(() => {
+    // close menu on click outside - set event type explicitly to mouse event
+    const handleClickOutside = (event: MouseEvent) => {
+      // treat event.target as node (has type EventTarget initially) - this gives it access to the contains method
+      // event.target is the dom element that was clicked on "mousedown"
+      // contains will be true for any element that is a child of the drop down ref div
+      //  false for elements that are not a child of the drop down rev div
+      if (
+        dropDownRef.current &&
+        !dropDownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // claean up event listener on component unmount
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
   }, []);
 
   return (
@@ -35,13 +63,22 @@ const UserMenu = () => {
         </div>
       </div>
       {isOpen && (
-        <div className="absolute right-0 top-12 w-[40vw] overflow-hidden rounded-xl bg-white text-sm shadow-md md:w-3/4">
+        <div
+          className="absolute right-0 top-12 w-[40vw] overflow-hidden rounded-xl bg-white text-sm shadow-md md:w-3/4"
+          ref={dropDownRef}
+        >
           <div className="flex cursor-pointer flex-col">
             <>
-              <MenuItem onClick={() => {}} label="Login" />
+              <MenuItem
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+                label="Login"
+              />
               <MenuItem
                 onClick={() => {
                   registerModal.onOpen();
+                  setIsOpen(!isOpen);
                 }}
                 label="Sign up"
               />
